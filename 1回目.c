d8d2a9dacfa0f3ed112d283d4480dc7b586b1eff
水野陽人 <mizuharu171017@gmail.com>
2025-10-10 19:09:33 +0900

    Add files via upload

---
+++ 1回目.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+ プログラム基礎 一回目
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int venom = 20051102;
+    printf("ウィリアムの生年月日は%dです\n", venom);//venomはかっこいい!!
+    venom = 20051103;
+    printf("あ、違えた%d",venom);
+
+    return 0;
+}
